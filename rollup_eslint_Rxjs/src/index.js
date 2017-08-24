58bdd33f56bdea083b991095d22bb7a25fbf26d7
malte hildebrand <malte.hildebrand@gmail.com>
2017-08-24 22:01:13 +0200

    fixed prblem with rxjs branch

---
+++ rollup_eslint_Rxjs/src/index.js
@@ -0,0 +1,9 @@
+
+// The logger should only be disabled if we’re not in production.
+ // if (ENV !== 'production') { ... }  spaeter so kapseln
+
+  document.write(
+    '<script src="http://' + (location.host || 'localhost').split(':')[0] +
+    ':35729/livereload.js?snipver=1"></' + 'script>'
+  );
+console.log('hello lovely');
